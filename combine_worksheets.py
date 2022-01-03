5c228026cb43cbbb599592c701a20f7629548367
Sven-Bo <jubbel3@gmx.net>
2022-01-03 13:42:01 +0100

    _

--- combine_worksheets.py
+++ combine_worksheets.py
@@ -2,22 +2,27 @@ from pathlib import Path  # Standard Python Module
 import time  # Standard Python Module
 import xlwings as xw  # pip install xlwings
 
-SOURCE_DIR = 'Files'
+# Adjust Paths
+BASE_DIR = Path(__file__).parent
+SOURCE_DIR = BASE_DIR / 'Files'
+OUTPUT_DIR = BASE_DIR / 'Output'
 
-excel_files = list(Path(SOURCE_DIR).glob('*.xlsx'))
-combined_wb = xw.Book()
+# Create output directory
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+excel_files = Path(SOURCE_DIR).glob('*.xlsx')
+
+# Create timestamp
 t = time.localtime()
 timestamp = time.strftime('%Y-%m-%d_%H%M', t)
 
-for excel_file in excel_files:
-    wb = xw.Book(excel_file)
-    for sheet in wb.sheets:
-        sheet.copy(after=combined_wb.sheets[0])
-    wb.close()
-
-combined_wb.sheets[0].delete()
-combined_wb.save(f'all_worksheets_{timestamp}.xlsx')
-if len(combined_wb.app.books) == 1:
-    combined_wb.app.quit()
-else:
+with xw.App(visible=False) as app:
+    combined_wb = app.books.add()
+    for excel_file in excel_files:
+        wb = app.books.open(excel_file)
+        for sheet in wb.sheets:
+            sheet.copy(after=combined_wb.sheets[0])
+        wb.close()
+    combined_wb.sheets[0].delete()
+    combined_wb.save(OUTPUT_DIR / f'all_worksheets_{timestamp}.xlsx')
     combined_wb.close()
